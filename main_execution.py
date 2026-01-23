b42d4448e6abe183c951c69e82c3d5f2eadfde28
ShinZoo <momo57140k@gmail.com>
2026-01-23 14:05:16 +0100

    test pour tester

--- main_execution.py
+++ main_execution.py
@@ -1,12 +1,66 @@
 # main.py
 from scrapper import scrap
+import undetected_chromedriver as uc
+import time
+from random import randrange
+
+import user_info
+
+user_agent = ["Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36", 
+              "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36", 
+              "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
+              "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
+              "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+              "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"]
+
+current_user = "default"
 
 if __name__ == "__main__":
+    totaltime = 0
+    totalproject = 0
+    scrappedproject = 0
     filename = "Followed_Projects.txt"
+
+    # initialisation
+    # config
+    options = uc.ChromeOptions()
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.binary_location = user_info.chrome_path
+
+    driver = uc.Chrome(options=options, version_main=142, use_subprocess=True, headless=False)
+
     with open(filename, 'r', encoding='utf-8') as fichier:
         for ligne in fichier:
             ligne = ligne.strip()
             if ligne:
-                scrap(ligne)
+                totalproject += 1
+                x, y = scrap(ligne, driver)
+                if x == 1:
+                    print("erreur with agent: "+ current_user + " spend : "+str(y))
+                else:
+                    scrappedproject += 1
+                totaltime += y
+            
+            if totalproject % 8 == 0:
+                driver.quit()
+                time.sleep(20)
+                options = uc.ChromeOptions()
+                options.add_argument("--window-size=1920,1080")
+                options.add_argument("--disable-gpu")
+                options.add_argument("--no-sandbox")
+                options.binary_location = user_info.chrome_path
+                current_user = user_agent[randrange(len(user_agent))]
+                print ("\n ###### \n"+current_user+"\n#####\n")
+                options.add_argument(f"--user-agent={current_user}")
+                driver = uc.Chrome(options=options, version_main=142, use_subprocess=True, headless=False, )
+    
+    driver.quit()
+
+    print("total :" + str(totalproject))
+    print("success :" + str(scrappedproject))
+    print("time :" + str(totaltime))
+    print("average :" + str(totaltime / scrappedproject))
 else:
     print("not executed as main")
--- scrapper.py
+++ scrapper.py
@@ -1,3 +1,5 @@
+import datetime
+
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,35 +8,43 @@ from selenium.webdriver import ActionChains
 import time
 import random
 import json
+
 import user_info
 
 # constantes
 TEMPS_PAUSE_MIN = 4 # temps de pause aléatoire au cas ou
-TEMPS_PAUSE_MAX = 5
+TEMPS_PAUSE_MAX = 8
+skip = True
 # Data to collect
-html = {"rewards": ["react-rewards-tab", ".col-span-12.col-span-8-md.col-span-9-lg.flex.flex-column.gap8.pt2.pt8-md"], "faq": ["project-faqs", ".mb5.grid-col-8-sm"], 
-        "updates": ["project-post-interface", ".grid-col-12.grid-col-8-md.grid-col-offset-2-md.mb6"], "comments": ["react-project-comments", ".text-center.bg-grey-200.p2.type-14"]}
+html = {"rewards": ["react-rewards-tab", ".col-span-12.col-span-8-md.col-span-9-lg.flex.flex-column.gap8.pt2.pt8-md"], 
+        "faq": ["project-faqs", ".mb5.grid-col-8-sm"], "updates": ["project-post-interface", ".grid-col-12.grid-col-8-md.grid-col-offset-2-md.mb6"], 
+        "comments": ["react-project-comments", ".text-center.bg-grey-200.p2.type-14"]}
 unused_html = {"creator": ["react-creator-tab", ".kds-flex.kds-items-center.kds-gap-05.kds-mb-06"],}
+current = {"state":"none"}
 
-
-def scrap_entete(driver: uc.Chrome, results):
+def scrap_entete(driver: uc.Chrome, results, current):
     
+    current["state"] = "entete"
     jsondata = driver.find_element(By.ID, "react-project-header").get_attribute("data-initial")
 
     if (jsondata):
         jsonloaded: dict = json.loads(jsondata)["project"]
-        results["creator"] = jsonloaded.pop("creator")
-        results["collaborators"] = jsonloaded.pop("collaborators")
+        tmp1 = jsonloaded.pop("creator")
+        tmp2 = jsonloaded.pop("collaborators")
         results["project"] = jsonloaded
+        results["creator"] = tmp1
+        results["collaborators"] = tmp2
+    else:
+        print("no json data")
 
-def scrap_description(driver: uc.Chrome, results):
-
+def scrap_description(driver: uc.Chrome, results, current):
     res = {
         "titre": [],
         "texte": [],
         "links": [],
         "imgs" : []
     }
+    current["state"] = "description"
     desc = driver.find_element(By.CSS_SELECTOR, ".rte__content.ck.ck-content").find_element("css selector", "div")
     for element in desc.find_elements("css selector", "p"):
         res["texte"].append(element.get_attribute('innerHTML'))
@@ -50,12 +60,13 @@ def scrap_description(driver: uc.Chrome, results):
 
     results["description"] = res
 
-def scrap_rewards(driver: uc.Chrome, results):
+def scrap_rewards(driver: uc.Chrome, results, current):
     res = {
         "available": [],
         "gone": []
     }
 
+    current["state"]  = "reward"
     containers = driver.find_elements(By.CSS_SELECTOR, "div.flex.flex-column.gap4")
 
     for container in containers:
@@ -64,49 +75,127 @@ def scrap_rewards(driver: uc.Chrome, results):
         if container.find_element(By.CSS_SELECTOR, ".support-700.normal.kds-heading.type-21.mb0.display-none-md").get_attribute("innerHTML") == "Available rewards":
             key = "available"
         for element in container.find_elements("css selector", "article"):
-            reward["logo"] = element.find_element("css selector", "img").get_attribute('src')
+            tmp = element.find_elements("css selector", "img")
+            reward["logo"] = tmp[0].get_attribute('src') if tmp else None
             reward["name"] = element.find_element("css selector", "header").find_element("css selector", "h3").get_attribute('innerHTML')
             reward["price"] = element.find_element(By.CSS_SELECTOR, ".support-700.type-18.m0.shrink0").get_attribute('innerHTML')
-            reward["delivery"] = element.find_element("css selector", "time").get_attribute('datetime')
-            reward["backers"] = int(str(element.find_element(By.CSS_SELECTOR, "div.flex.items-center.gap4px > span[aria-label]").get_attribute('innerHTML')))
-            reward["desc"] = element.find_element(By.CSS_SELECTOR, ".type-16.support-700.text-prewrap").get_attribute('innerHTML')
+            tmp = element.find_elements("css selector", "time")
+            reward["delivery"] = tmp[0].get_attribute('datetime') if tmp else None
+            tmp = element.find_elements(By.CSS_SELECTOR, "div.flex.items-center.gap4px > span[aria-label]")
+            reward["backers"] = int(str(tmp[0].get_attribute('innerHTML'))) if tmp else 0
+            tmp = element.find_elements(By.CSS_SELECTOR, ".type-16.support-700.text-prewrap")
+            reward["desc"] = tmp[0].get_attribute('innerHTML') if tmp else None
             tmp = element.find_elements(By.XPATH, ".//h3[contains(text(), 'Limited quantity')]/following-sibling::div[@class='type-14']")
             reward["left"] = tmp[0].get_attribute("innerHTML") if tmp else None
             tmp = element.find_elements(By.XPATH, ".//h3[contains(text(), 'Ships to')]/following-sibling::div[@class='type-14']")
             reward["shipping"] = tmp[0].get_attribute("innerHTML") if tmp else None
             reward["items"] = []
-            for item in element.find_element(By.CSS_SELECTOR, ".flex.flex-column.gap1").find_elements(By.CSS_SELECTOR, ".border.border-support-700.mb3.py3.px3.radius4px.clip"):
-                blob = {}
-                blob["name"] = item.find_element("css selector", "h3").get_attribute('innerHTML')
-                blob["quantity"] = item.find_element("css selector", "p").get_attribute('innerHTML')
-                reward["items"].append(blob)
+            if element.find_elements(By.CSS_SELECTOR, ".flex.flex-column.gap1"):
+                for item in element.find_element(By.CSS_SELECTOR, ".flex.flex-column.gap1").find_elements(By.CSS_SELECTOR, ".border.border-support-700.mb3.py3.px3.radius4px.clip"):
+                    blob = {"name": item.find_element("css selector", "h3").get_attribute('innerHTML')}
+                    quant = item.find_elements("css selector", "p")
+                    blob["quantity"] = quant[0].get_attribute('innerHTML') if quant else None
+                    reward["items"].append(blob)
             reward["options"] = []
-            for option in element.find_element(By.CSS_SELECTOR, ".mt4.flex.flex-column.gap1").find_elements(By.CSS_SELECTOR, ".border.border-support-700.mb3.py3.px3.radius4px.clip"):
-                blob = {}
-                blob["name"] = option.find_element("css selector", "h3").get_attribute('innerHTML')
-                blob["price"] = option.find_element("css selector", "p").get_attribute('innerHTML')
-                blob["desc"] = option.find_element(By.CSS_SELECTOR, ".type-14.lh20px.mb0.support-700").get_attribute('innerHTML')
-                reward["options"].append(blob)
+            if element.find_elements(By.CSS_SELECTOR, ".mt4.flex.flex-column.gap1"):
+                for option in element.find_element(By.CSS_SELECTOR, ".mt4.flex.flex-column.gap1").find_elements(By.CSS_SELECTOR, ".border.border-support-700.mb3.py3.px3.radius4px.clip"):
+                    blob = {}
+                    blob["name"] = option.find_element("css selector", "h3").get_attribute('innerHTML')
+                    tmp = option.find_elements("css selector", "p")
+                    blob["price"] = tmp[0].get_attribute('innerHTML') if tmp else None
+                    desc = option.find_elements(By.CSS_SELECTOR, ".type-14.lh20px.mb0.support-700")
+                    blob["desc"] = desc[0].get_attribute('innerHTML') if desc else None
+                    reward["options"].append(blob)
             res[key].append(reward.copy())
     results["rewards"] = res
 
 # Unused as creator info are collected in the head
-def scrap_creator(driver: uc.Chrome, results):
+def scrap_creator(driver: uc.Chrome, results, current):
     res = {
         "tags" : "kds-flex kds-flex-wrap kds-gap-02" # repeat creator | super backer...
     }
 
-def scrap_faq(driver: uc.Chrome, results):
-    
-    pass
+def scrap_faq(driver: uc.Chrome, results, current):
+    res = []
+    results["faq"] = res
 
-def scrap_updates(driver: uc.Chrome, results):
-    
-    pass
+    current["state"] = "faq"
 
-def scrap_comments(driver: uc.Chrome, results):
+    faq = driver.find_elements(By.CSS_SELECTOR, ".type-14.navy-700.medium")
+
+    if not faq : return 
+
+    faq = driver.find_element(By.ID, "project-faqs").find_element(By.CSS_SELECTOR, "ul").find_elements(By.CSS_SELECTOR, "li")
+
+    for element in faq:
+        question = {}
+        question["question"] = element.find_element("css selector", ".type-14.navy-700.medium").get_attribute('innerHTML')
+        question["answer"] = []
+        for part in element.find_elements("css selector", "p"):
+            question["answer"].append(part.get_attribute('innerHTML'))
+        question["lastUpdated"] = element.find_element("css selector", "time").get_attribute('datetime')
+        res.append(question.copy())
+
+    results["faq"] = res
+
+def scrap_updates(driver: uc.Chrome, results, current):
+    res = []
+
+    current["state"] = "update"
+
+    container = driver.find_elements(By.CSS_SELECTOR, "truncated-post soft-black block")
+
+    for element in container:
+        update = {}
+
+        update["name"] = element.find_element(By.CSS_SELECTOR, ".kds-mb-04.kds-type.kds-type-heading-xl").get_attribute('innerHTML')
+        update["uploadTime"] = element.find_element(By.CSS_SELECTOR, ".kds-text-secondary.kds-type.kds-type-body-sm").get_attribute('innerHTML')
+        
+        res.append(update.copy())
     
-    pass
+    res.reverse()
+    results["updates"] = res
+
+def scrap_comments(driver: uc.Chrome, results, current):
+    res = []
+
+    current["state"] = "comments"
+
+    tmp = driver.find_elements(By.CSS_SELECTOR, ".bg-grey-100.border.border-grey-400.p2.mb3")
+    if not tmp : 
+        print("no comments")
+        results["comments"] = res
+        return
+
+    comments = tmp[0].find_elements(By.CSS_SELECTOR, ":scope > li")
+
+    for element in comments:
+        comment = {}
+        tmp = element.find_elements(By.CSS_SELECTOR, ":scope > div")
+        if not tmp : continue
+        pseudo = tmp[0].find_elements(By.CSS_SELECTOR, "span.mr2")
+        if pseudo:
+            comment["pseudo"] = pseudo[0].find_element(By.CSS_SELECTOR, "span").get_attribute("innerHTML")
+            comment["uploadTime"] = tmp[0].find_element(By.CSS_SELECTOR, "time").get_attribute('datetime')
+        else:
+            comment["pseudo"] = None
+            comment["uploadTime"] = None
+        comment["text"] = tmp[0].find_element(By.CSS_SELECTOR, "p").get_attribute('innerHTML')
+        comment["replies"] = []
+        for reply in element.find_elements(By.CSS_SELECTOR, "li"):
+            rep = {}
+            pseudo = reply.find_elements(By.XPATH, ".//a[@class='comment-link']/preceding-sibling::span[@class='mr2']")
+            if pseudo:
+                rep["pseudo"] = pseudo[0].find_element(By.CSS_SELECTOR, "span").get_attribute("innerHTML")
+                rep["uploadTime"] = reply.find_element(By.CSS_SELECTOR, "time").get_attribute('datetime')
+            else:
+                rep["pseudo"] = None
+                rep["uploadTime"] = None
+            rep["text"] = reply.find_element(By.CSS_SELECTOR, "p").get_attribute('innerHTML')
+            comment["replies"].append(rep.copy())
+        res.append(comment.copy())
+    
+    results["comments"] = res
 
 scrap_functions = {
     "rewards": scrap_rewards,
@@ -115,18 +204,10 @@ scrap_functions = {
     "updates": scrap_updates,
 }
 
-def scrap(url):
-    # initialisation
-    # config
-    options = uc.ChromeOptions()
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.binary_location = user_info.chrome_path
-
-    driver = uc.Chrome(options=options, version_main=142, use_subprocess=True, headless=False)
+def scrap(url, driver):
 
     start_time = time.time()
+    total_time = 0
     results = {}
     try:
         print("Scraping project")
@@ -143,13 +224,13 @@ def scrap(url):
         )
 
         # Cherche l'entete, l'element react-project-header n'existe pas si le projet est finit
-        element = driver.find_element(By.ID, "react-project-header")
+        element = driver.find_elements(By.ID, "react-project-header")
         if element:
-            scrap_entete(driver, results)
+            scrap_entete(driver, results, current)
         else:
             results["head"] = "<p>No Head, project ended</p>"
         
-        scrap_description(driver, results)
+        scrap_description(driver, results, current)
         
         ###########################################################################
 
@@ -161,25 +242,26 @@ def scrap(url):
         #                   Parcourir les pages du projet                         #
         ###########################################################################
         for key in html.keys():
+
+            if (key == "faq" or key == "updates") and skip:
+                continue
         
-            print("scrap : "+ str(key))
+            #print("scrap : "+ str(key))
 
             # Trouver l'onglet de la page et cliquer dessus
             driver.find_element(By.ID, key+"-emoji").click()
 
-            print("waiting for the css class")
             # Attendre jusqu'à 10 secondes que le contenue de la balise apparaisse (json de l'onglet terminé)
             WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, str(html[key][1])))
             )
-            print("balise css found")
 
             # on fait une pause d'une durée aléatoire pour réduire les chances d'être perçu comme un bot
             pause = random.uniform(TEMPS_PAUSE_MIN, TEMPS_PAUSE_MAX)
             time.sleep(pause)
 
             # Appel la fonction de scrap correspondante
-            scrap_functions.get(key, scrap_comments)(driver, results)
+            scrap_functions.get(key, scrap_comments)(driver, results, current)
 
             # Mouvement de souris imittant un comportement humain 
             iframe = driver.find_element(By.ID, str(html[key][0]))
@@ -196,27 +278,38 @@ def scrap(url):
                 .perform()
             
             ActionChains(driver)\
-                .scroll_by_amount(0, random.randrange(20,100))\
+                .scroll_by_amount(0, random.randrange(100,300))\
                 .perform()
         
-        total_time= time.time() - start_time
+        total_time = time.time() - start_time
         print("Projet collecté en "+ str(total_time) +"\n")
 
     except Exception as e:
-        print(f"Erreur : {e.__cause__}")
+        print(f"Erreur : {e.__traceback__}")
+        print("#### "+current["state"] +" ####")
+        return 1, time.time() - start_time
 
     finally:
-        driver.quit()
 
-
-    # sauvegarder les projets
-    if results:
-        name: str = "Downloaded_html/"+ str(round(time.time())) +".json"
-        # Écriture du dictionnaire dans un fichier JSON
-        with open(name, "w", encoding="utf-8") as fichier:
-            json.dump(results, fichier, indent=4)
-    else:
-        print("Projet non collecté \n")
+        # sauvegarder les projets
+        if results:
+            name: str = datetime.now().strftime("%d-%m-%Y") + "_" + str(round(time.time())) + ".json"
+            # Écriture du dictionnaire dans un fichier JSON
+            with open(name, "w", encoding="utf-8") as fichier:
+                json.dump(results, fichier, indent=4)
+        else:
+            print("Projet non collecté \n")
+    
+    return 0, total_time
 
 if __name__ == "__main__":
-    scrap("https://www.kickstarter.com/projects/elocinsolis/stella-the-shiba-inu-acrylic-keychains")
+    # initialisation
+    # config
+    options = uc.ChromeOptions()
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.binary_location = user_info.chrome_path
+
+    driver = uc.Chrome(options=options, version_main=142, use_subprocess=True, headless=False)
+    scrap("https://www.kickstarter.com/projects/lifespirittarot/bricks-of-fate-tarot-built-brick-by-brick", driver)
