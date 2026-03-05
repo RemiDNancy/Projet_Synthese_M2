fcf78059a7e83758a2802910d12384a4221ff135
mohamed <mohamed.krouchi01@gmail.com>
2026-03-05 21:40:35 +0100

    correction script index

--- Ingest/BDD_V3.sql
+++ Ingest/BDD_V3.sql
@@ -1,4 +1,3 @@
-
 USE kickstarter;
 
 DROP TABLE IF EXISTS REWARD_EVOLUTION;
@@ -12,11 +11,11 @@ DROP TABLE IF EXISTS CREATOR;
 -- Création de la table CREATOR
 
 CREATE TABLE CREATOR (
-    creator_id VARCHAR(255) PRIMARY KEY, 
+    creator_id VARCHAR(255) PRIMARY KEY,
     creator_name VARCHAR(255),
     biography TEXT,
     launched_projects_count INT,
-    backings_count INT, 
+    backings_count INT,
     is_fb_connected BOOLEAN,
     nb_websites INT,
     last_login INT
@@ -45,7 +44,7 @@ CREATE TABLE PROJECT (
 CREATE INDEX idx_project_creator ON PROJECT(id_creator);
 CREATE INDEX idx_project_category ON PROJECT(category);
 
--- Création de la table PROJECT_EVOLUTION ce qui change dans projet 
+-- Création de la table PROJECT_EVOLUTION ce qui change dans projet
 
 CREATE TABLE PROJECT_EVOLUTION (
     evolution_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -61,7 +60,7 @@ CREATE TABLE PROJECT_EVOLUTION (
     CONSTRAINT uq_project UNIQUE (project_id, scrap_date)
 );
 
-CREATE INDEX idx_pe_project_date ON PROJECT_EVOLUTION(project_id, scrap_datetime);
+CREATE INDEX idx_pe_project_date ON PROJECT_EVOLUTION(project_id, scrap_date);
 CREATE INDEX idx_pe_state ON PROJECT_EVOLUTION(current_state);
 
 -- Création de la table PROJECT_COMMENT
@@ -91,7 +90,7 @@ CREATE TABLE REWARD (
     price_amount DECIMAL(15, 2),
     estimated_delivery DATE,
     CONSTRAINT fk_reward_project FOREIGN KEY (project_id) REFERENCES PROJECT(project_id)
-    
+
 );
 
 CREATE INDEX idx_r_project ON REWARD(project_id);
@@ -101,7 +100,7 @@ CREATE TABLE REWARD_ITEM (
     item_id INT PRIMARY KEY AUTO_INCREMENT,
     reward_id INT NOT NULL,
     item_name VARCHAR(255) NOT NULL,
-    item_quantity VARCHAR(50),                                  -- "50" format du JSON Donc il faut faire un pretraitement ici avec le python                                                   
+    item_quantity VARCHAR(50),                                  -- "50" format du JSON Donc il faut faire un pretraitement ici avec le python
     CONSTRAINT fk_item_reward FOREIGN KEY (reward_id) REFERENCES REWARD(reward_id)
         ON DELETE CASCADE
 );
@@ -112,7 +111,7 @@ CREATE TABLE REWARD_OPTION (
     option_id INT PRIMARY KEY AUTO_INCREMENT,
     reward_id INT NOT NULL,
     option_name VARCHAR(255) NOT NULL,
-    option_price VARCHAR(50),                                   -- "+€5" format du JSON Donc il faut faire un pretraitement ici avec le python 
+    option_price VARCHAR(50),                                   -- "+€5" format du JSON Donc il faut faire un pretraitement ici avec le python
     option_description TEXT,
     CONSTRAINT fk_option_reward FOREIGN KEY (reward_id) REFERENCES REWARD(reward_id)
         ON DELETE CASCADE
@@ -125,16 +124,16 @@ CREATE TABLE REWARD_EVOLUTION (
     reward_evolution_id INT PRIMARY KEY AUTO_INCREMENT,
     reward_id INT NOT NULL,                          -- NOT NULL
     scrap_date DATETIME NOT NULL,                       -- NOT NULL
-    
-    
+
+
     remaining_quantity INT,
     backers_on_reward INT DEFAULT 0,                    -- defaukt = 0
-    
+
     -- Variations journalières pour l'analyse
     daily_backers_variation INT,
 
     CONSTRAINT fk_rev_hist FOREIGN KEY (reward_id) REFERENCES REWARD(reward_id),
-        
+
         -- anti-doublon (1 snapshot par reward par datetime)
     UNIQUE KEY uq_reward_snapshot (reward_id, scrap_date)
 );
